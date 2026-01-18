b4e2641e2d221344be7112df6cdde244e8f453f8
viljardh <viljardh@gmail.com>
2026-01-18 21:30:19 +0100

    add appdatabase i think

---
+++ app/src/main/java/com/example/sausdb/data/database/AppDatabase.kt
@@ -0,0 +1,39 @@
+package com.example.sausdb.data.database
+
+import android.content.Context
+import android.util.Log
+import androidx.room.AutoMigration
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [RecipeEntity::class],
+    version = 1,
+    exportSchema = true,
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun recipeDao(): RecipeDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            Log.d("AppDatabase", "Initializes database")
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext, AppDatabase::class.java, "placeinfo_database"
+                ).createFromAsset("database/placeinfo_database.db").build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
